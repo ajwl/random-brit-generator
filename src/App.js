6a02f449291005f9555253dd26a12d38ddbd7700
Anna <ajwleach@gmail.com>
2017-08-13 18:01:44 +0100

    different components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {getPeople} from './generator.js';
-
-const people = getPeople(3);
+import NumberInput from './NumberInput.js'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {numberPeople: 3, people: ''};
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNumberChange(e){
+    this.setState({ numberPeople: e.target.value });
+  };
+
+  handleSubmit(e){
+    this.setState((prevState, props) => {
+    return {
+      numberPeople: getNumberPeople(prevState.numberPeople)
+    } });
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,8 +31,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Who are the people?</h2>
         </div>
+        <form onSubmit={this.handleSubmit} className="input-number">
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Get People" />
+        </form>
         <ul className="App-intro">
-          {people.map((person) => {
+          {this.state.people.map((person) => {
             return <li>{person['gender']} + {person.age} + {person.ethnicity}</li>;
           })}
         </ul>
--- src/NumberInput.js
+++ src/NumberInput.js
@@ -1,3 +1,35 @@
+import React, { Component } from 'react';
 
+class NumberInput extends Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.getPeople = this.getPeople.bind(this);
+  }
 
-export default Number;
+  handleChange(e){
+    console.log("this fired correctly")
+    this.props.handleNumberChange(e.target.value);
+  }
+
+  handleSubmit(e){
+    this.props.getPeople()
+    e.preventDefault();
+  }
+
+  getPeople(){
+    this.props.getThosePeople();
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit} className="input-number">
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Get People" />
+      </form>
+    )
+  }
+
+}
+
+export default NumberInput;
