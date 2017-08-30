9b263c9696f3eee80726b08b42890d9a3756c155
Anna <ajwleach@gmail.com>
2017-08-30 08:36:05 +0100

    routing

---
+++ src/components/containers/Fruitmachine.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Fruitmachine extends Component {
+
+  render(){
+    return(
+      <div>
+        <h2> heloooooo </h2>
+      </div>
+    )
+  }
+}
+
+export default Fruitmachine;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Root from './Root';
 import App from './components/App';
+import Fruitmachine from './components/containers/Fruitmachine';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={App} />
+      <Route path='/machine' component={Fruitmachine} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
